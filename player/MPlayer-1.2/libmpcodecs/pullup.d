a02e3592bd64d13ecf91f42317037c0caca835cb
JiangShen <iplaycloud@gmail.com>
2016-01-04 17:49:45 +0800

    add mpg123

---
+++ player/MPlayer-1.2/libmpcodecs/pullup.d
@@ -0,0 +1,8 @@
+libmpcodecs/pullup.o: libmpcodecs/pullup.c ffmpeg/libavutil/x86/asm.h \
+ config.h libmpcodecs/pullup.h
+
+ffmpeg/libavutil/x86/asm.h:
+
+config.h:
+
+libmpcodecs/pullup.h:
---
+++ player/MPlayer-1.2/version.h
@@ -0,0 +1,2 @@
+#define VERSION "1.2-4.4.7"
+#define MP_TITLE "%s "VERSION" (C) 2000-2015 MPlayer Team\n"
